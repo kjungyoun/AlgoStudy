b697ba121367c0bb55b38314de4a66ee3349270f
kjungyoun <rlawjddbs963@naver.com>
2021-04-09 01:00:02 +0900

    04.08 Algoritm Study

--- 4월/04.11/Main_S2_1541_잃어버린괄호.java
+++ 4월/04.11/Main_S2_1541_잃어버린괄호.java
@@ -1,26 +1,3 @@
-#🌹 04.11 - 백준_S2_1541_잃어버린 괄호
-
-## 📝 문제 주소
-
-[백준 1976번: 여행 가자](https://www.acmicpc.net/problem/1541)
-
-## 📝 알고리즘 분류
-
-- 수학
-- 그리디 알고리즘
-- 문자열
-- 파싱
-
-## ⁉️ 알고리즘 풀이
-
-- 해당 문제를 해결하기 위한 알고리즘 입력
-- split을 이용할 때 특수 문자를 인자로 넣을 경우 반드시 \\ 를 붙여주어야 한다!!
-
-![스크린샷 2021-04-09 오전 12 55 20](https://user-images.githubusercontent.com/48318620/114058492-734c5780-98ce-11eb-8db5-60d2652c0b51.png)
-
-## 📜 코드
-
-```java
 package com.beak;
 
 import java.io.BufferedReader;
@@ -52,4 +29,3 @@ public class Main_S2_1541_잃어버린괄호 {
 	}
 
 }
-```
